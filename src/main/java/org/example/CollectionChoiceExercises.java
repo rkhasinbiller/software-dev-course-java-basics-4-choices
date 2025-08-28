86111750d2925318ac6046712d61523c5a47d810
rkhasinbiller <rkhasinbiller@gmail.com>
2025-08-28 15:17:00 -0500

    Added example exercise and improved instructions.

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -8,29 +8,19 @@ package org.example;
 // If you get stuck, refer to the java basics 4 (arrays and collections) curriculum
 public class CollectionChoiceExercises {
     public void MakeColorCollection() {
-        // EXAMPLE:
-        // Create an appropriate variable for holding a collection
-        // of three strings that are known ahead of time to be "Red", "Green" and "Blue"
-        // and will not change during program execution.
 
         String[] colors;
     }
 
     public void MakeStudentCollection() {
-        // Create an appropriate collection variable for holding a list of student names
-        // that will be added to and removed from during program execution.
+        ArrayList<String> students;
     }
 
     public void MakeLoggedInUsersCollection() {
-        // Create an appropriate collection variable for holding a set of
-        // unique logged in usernames where adding a username that's already
-        // present is ignored and checking if a user is in the collection can
-        // be done very quickly.
+        HashSet<String> loggedInUsers;
     }
 
     public void MakeEmployeeAccessCodesCollection() {
-        // Create an appropriate collection variable for holding a set of
-        // key-value pairs where they key is an employee name (String) and the
-        // value is an employee access code (String.)
+        HashMap<String, String> employeeAccessCodes;
     }
 }
